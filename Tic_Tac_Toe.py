90d0bc01b547b8309ace98b51b4e95680ba9b3e8
JayDee91manasa <jdmanasa.codingal@gmail.com>
2025-11-10 00:14:45 +0530

    Refactor Tic Tac Toe game implementation
    indentation error is fixed

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -26,81 +26,80 @@ def printBoard(board):
 # Now we'll write the main function which has all the gameplay functionality.
 def game():
   
-  turn='X'
-  count=0
+    turn='X'
+    count=0
 
-  for i in range(10):
-    printBoard(theBoard)
-    print("It's your turn," + turn + ".Move tow which place?")
+    for i in range(10):
+        printBoard(theBoard)
+        print("It's your turn," + turn + ".Move tow which place?")
 
-  move=input()
-   if theBoard[move] ==' ':
-    theBoard[move] = turn
-    count += 1
-  else:
-    print("That place is already filled.\nMove to which place?")
-    continue
+        move=input()
 
+        if theBoard[move] ==' ':
+            theBoard[move] = turn
+            count += 1
+        else:
+            print("That place is already filled.\nMove to which place?")
+            continue
+     #will move 
 
-if count>= 5:
-    if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
-        printBoard(theBoard)
-        print("\nGame Over.\n")
-        print(" *** " +turn + " won. ***")
-        break
-    elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ': #across the middle
-        printBoard(theBoard)
-        print("\nGame Over.\n")
-        print(" *** " +turn + " won. ***")
-        break
-    elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ': #across the bottom
-        printBoard(theBoard)
-        print("\nGame Over.\n")
-        print(" *** " +turn + " won. ***")
-        
-    elif theBoard['1'] == theBoard['4'] == theBoard['7'] != ' ': # down the left side
-        printBoard(theBoard)
-        print("\nGame Over.\n")
-        print(" *** " +turn + " won. ***")
-        break
-    elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ': # down the middle
-        printBoard(theBoard)
-        print("\nGame Over.\n")
-        print(" *** " +turn + " won. ***")
-        break
-    elif theBoard['3'] == theBoard['6'] == theBoard['9'] != ' ': # down the right side
-        printBoard(theBoard)
-        print("\nGame Over.\n")
-        print(" *** " +turn + " won. ***")
-        break
-    elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ': # diagonal
-        printBoard(theBoard)
-        print("\nGame Over.\n")
-        print(" *** " +turn + " won. ***")
-        break
-    elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ': # diagonal 
-        printBoard(theBoard)
-        print("\nGame Over.\n")
-        print(" *** " +turn + " won. ***")
+        if count>= 5:
+            if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print(" *** " +turn + " won. ***")
+                break
+            elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ': #across the middle
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print(" *** " +turn + " won. ***")
+                break
+            elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ': #across the bottom
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print(" *** " +turn + " won. ***")
+            elif theBoard['1'] == theBoard['4'] == theBoard['7'] != ' ': # down the left side
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print(" *** " +turn + " won. ***")
+                break
+            elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ': # down the middle
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print(" *** " +turn + " won. ***")
+                break
+            elif theBoard['3'] == theBoard['6'] == theBoard['9'] != ' ': # down the right side
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print(" *** " +turn + " won. ***")
+                break
+            elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ': # diagonal
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print(" *** " +turn + " won. ***")
+                break
+            elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ': # diagonal 
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print(" *** " +turn + " won. ***")
+                break
         
     
 # if neither X nor O wins and the board is full, we'll declare the result as 'tie'.
-if count == 9:
-    print("\nGame Over.\n")
-    print("It's a Tie!!")
+        if count == 9:
+            print("\nGame Over.\n")
+            print("It's a Tie!!")
 
-if turn == 'X':
-    turn='O'
-else:
-    turn='X'
+        if turn == 'X':
+            turn='O'
+        else:
+            turn='X'
 
-restart = input("Do you want to play Again?(y/n)")
-if restart == "y" or restart == "Y":
-    for key in board_keys:
-        theBoard[key] = " "
+    restart = input("Do you want to play Again?(y/n)")
+    if restart == "y" or restart == "Y":
+        for key in board_keys:
+            theBoard[key] = " "
+            game()   
 
-    
-    ()
-
-"if__name__"=="__main__"
-()
+if __name__ == "__main__":
+    game()
